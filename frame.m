fec52dcddcf39a90336ac0bd3e38696fe9ded6e3
Dan Ellis <dpwe@ee.columbia.edu>
2014-03-18 11:39:01 -0700

    only returns full windows; uses bsxfun not repmat

--- frame.m
+++ frame.m
@@ -5,15 +5,20 @@ function [Y,winix] = frame(X,W,H)
 %   There's no windowing (i.e. scaling by a tapered window) in here. 
 % 2010-11-14 Dan Ellis dpwe@ee.columbia.edu
 
-lx = length(X);
-nh = 1+ceil((length(X)-W)/H);
-% Pad X to an integral number of windows
-Xp = [X(:)',zeros(1, (W+(nh-1)*H)-lx)];
+%lx = length(X);
+%nh = 1+ceil((length(X)-W)/H);
+%% Pad X to an integral number of windows
+%Xp = [X(:)',zeros(1, (W+(nh-1)*H)-lx)];
+% No, truncate to have only whole windows
+nh = 1+floor((length(X)-W)/H);
+Xp = [X(1:W+(nh-1)*H)'];
 
 % Index-fu:
 % We build a matrix of indices that pull out the values we want...
 % (columns of 1:W, with successive multiples of H added on)
-winix = repmat(H*[0:(nh-1)],W,1)+repmat([1:W]',1,nh);
+%winix = repmat(H*[0:(nh-1)],W,1)+repmat([1:W]',1,nh);
+winix = bsxfun(@plus, repmat([1:W]',1,nh), H*[0:(nh-1)]);
+%winix = bsxfun(@plus, repmat(H*[0:(nh-1)],W,1), [1:W]');
 
 % .. then the output is just the input matrix indexed by these indices
 Y = Xp(winix);
